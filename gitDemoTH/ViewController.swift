af462a6dd6e4c513e154949b2cf137ea800941f6
Miguel Rodriguez <miguelrodriguez@miguels-macbook-pro.local>
2016-05-25 09:58:51 -0500

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Miguel Rodriguez on 5/25/16.
+//  Copyright © 2016 Salefer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
